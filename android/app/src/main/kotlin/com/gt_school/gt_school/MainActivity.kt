6922d54a1ac58d29667f6cddb0a0136353275ab4
Milos Nikolic <milos5611@gmail.com>
2024-05-12 22:25:08 +0200

    init

---
+++ android/app/src/main/kotlin/com/gt_school/gt_school/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.gt_school.gt_school
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/widgets/system_device_tile.dart
@@ -0,0 +1,92 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_blue_plus/flutter_blue_plus.dart';
+
+class SystemDeviceTile extends StatefulWidget {
+  final BluetoothDevice device;
+  final VoidCallback onOpen;
+  final VoidCallback onConnect;
+
+  const SystemDeviceTile({
+    required this.device,
+    required this.onOpen,
+    required this.onConnect,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<SystemDeviceTile> createState() => _SystemDeviceTileState();
+}
+
+class _SystemDeviceTileState extends State<SystemDeviceTile> {
+  BluetoothConnectionState _connectionState = BluetoothConnectionState.disconnected;
+  int? _rssi;
+
+  late StreamSubscription<BluetoothConnectionState> _connectionStateSubscription;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // asyncMethod();
+    widget.device.readRssi().then((value) {
+      _rssi = value;
+    });
+
+    _connectionStateSubscription = widget.device.connectionState.listen((state) {
+      _connectionState = state;
+      if (mounted) {
+        setState(() {});
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _connectionStateSubscription.cancel();
+    super.dispose();
+  }
+
+  bool get isConnected {
+    return _connectionState == BluetoothConnectionState.connected;
+  }
+
+  Widget _buildAdvRow(BuildContext context, String title, String value) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 4.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(title, style: Theme.of(context).textTheme.bodySmall),
+          const SizedBox(
+            width: 12.0,
+          ),
+          Expanded(
+            child: Text(
+              value,
+              style: Theme.of(context)
+                  .textTheme
+                  .bodySmall
+                  ?.apply(color: Colors.black),
+              softWrap: true,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ExpansionTile(
+      leading: Text(_rssi.toString()),
+      title: Text(widget.device.platformName),
+      subtitle: Text(widget.device.remoteId.str),
+      children: <Widget>[
+        if (widget.device.platformName.isNotEmpty)
+          _buildAdvRow(context, 'Name', widget.device.platformName),
+      ],
+    );
+  }
+}
